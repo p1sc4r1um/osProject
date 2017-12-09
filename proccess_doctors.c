ae6a92088140a412f963aceb8391a0d6ad7c6b17
p1sc4r1um <p1sc4r1um@github.com>
2017-12-09 16:49:37 +0000

    doctors

--- proccess_doctors.c
+++ proccess_doctors.c
@@ -7,39 +7,38 @@ void kill_proc() {
 
 void doctor (int shift_length) {
 	MQ_patient patient;
-	double time_patient;
+	double time_patient, total_time;
 	signal(SIGALRM, kill_proc);
 	ualarm(shift_length, 0);
 	clock_t start = clock();
-	double shift_time = (double)(end - start) / CLOCKS_PER_SEC;
+	//double shift_time = (double)(end - start) / CLOCKS_PER_SEC;
 	printf("I am doctor %ld and started at %f\n", (long)getpid(), (double) start / CLOCKS_PER_SEC);
- 	while (((int)shift_time < shift_length)) {
+ 	//while (((int)shift_time < shift_length)) {
+	while(1) {	
+		signal(SIGALRM, SIG_IGN);
 		msgrcv(MQ_id, &patient, sizeof(MQ_patient)-sizeof(long), -3, 0);
 		current->begin_attendance = clock();
 		usleep(current->attendancems);
 		clock_t end = clock();
-		shift_time = (double)(end - start) / CLOCKS_PER_SEC;
+		//shift_time = (double)(end - start) / CLOCKS_PER_SEC;
 		time_patient = 1.0 * (current->begin_attendance - current->end_triage)/CLOCKS_PER_SEC;
 	 	sem_wait(mutex);
 		(*shared_var).total_treated++;
-		(*shared_var).average_after_triage = ((*shared_var).average_after_triage * ((*shared_var).total_treated-1)) + (time_patient * 1000))/(*shared_var).total_treated;
-	 	sem_post(mutex);
+		(*shared_var).average_after_triage = ((*shared_var).average_after_triage * ((*shared_var).total_treated-1)) + (time_patient * 1000)/(*shared_var).total_treated;
+		total_time = 1.0* (end - current->start)/CLOCKS_PER_SEC;
+	 	(*shared_var).average_all = ((*shared_var).average_all * ((*shared_var).total_treated-1)) + (total_time * 1000)/(*shared_var).total_treated; 
+		sem_post(mutex);
 
 	 //printf("total treated: %d\n", (*shared_var).total_treated);
-	 /*TODO: solve patient*/
-
-//     int time_doc = end / CLOCKS_PER_SEC;
-//	 (*shared_var).average_after_triage = ((*shared_var).average_after_triage * ((*shared_var).total_treated-1) + time_doc-patient.end_triage)/(*shared_var).total_treated;
-//	 (*shared_var).average_all = ((*shared_var).average_all * ((*shared_var).total_treated-1) + time_doc-patient.start)/(*shared_var).total_treated;
-
-    #ifdef DEBUG
-     printf("DOCTOR - start: %d\n", patient.end_triage);
-     printf("DOCTOR - end: %d\n", time_doc);
-     printf("DOCTOR - shift duration: %d\n", shift_time);
-     printf("DOCTOR - average_after_triage: %d\n", (*shared_var).average_after_triage);
-     printf("DOCTOR - average_all: %d\n", (*shared_var).average_all);
-    #endif
- }
+		signal(SIGALRM, kill_proc);
+		#ifdef DEBUG
+		 printf("DOCTOR - start: %d\n", patient.end_triage);
+		 printf("DOCTOR - end: %d\n", time_doc);
+		 printf("DOCTOR - shift duration: %d\n", shift_time);
+		 printf("DOCTOR - average_after_triage: %d\n", (*shared_var).average_after_triage);
+		 printf("DOCTOR - average_all: %d\n", (*shared_var).average_all);
+		#endif
+	}
 }
 
 void create_doctors (int doctors, int shift_length) {
